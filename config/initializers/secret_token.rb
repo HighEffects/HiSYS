bbd10f664676ec13a5f0daab8a947317c4d30c68
James Peret <james.peret@gmail.com>
2012-12-19 19:25:12 -0200

    Basic page scaffold and devise user login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hisys::Application.config.secret_token = 'a98fb6f56ed8e4fd388acf46ae65c53284a8557528c4ea197432bbd5eb4cf33122ae1b8695f8952d5517124e563daf53c3f4c123586d4da9a5f0083551e64c8b'
