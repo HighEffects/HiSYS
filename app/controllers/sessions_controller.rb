03dc4af578af238b03457e0f0f5025fca99eff90
James Peret <james.peret@gmail.com>
2013-02-11 13:34:11 -0200

    Mixpanel analytics update and page list UI correction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,10 @@ class SessionsController < Devise::SessionsController
   def create
     super
     if Rails.env.production?
-       
+      mixpanel.track 'User login', { :user_id => current_user.id }
       username = current_user.first_name + " " + current_user.last_name
       mixpanel.set current_user.id, { :name => username, :email => current_user.email}
       mixpanel.increment current_user.id, { :logins => 1 }
-      mixpanel.track 'User login'
     end
   end
 
