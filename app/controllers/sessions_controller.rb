865a1a1dccf439389a2904446ef7f7e0422bc7f4
James Peret <james.peret@gmail.com>
2013-02-11 05:02:10 -0200

    Mixpanel user login analytics

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,12 @@ class SessionsController < Devise::SessionsController
 
   def create
     super
-    mixpanel.track 'User login' if Rails.env.production?
+    if Rails.env.production?
+      mixpanel.track 'User login' 
+      username = current_user.first_name + " " + current_user.last_name
+      mixpanel.set current_user.id, { :name => username, :email => current_user.email, :last_login => Time.now }
+      mixpanel.increment current_user.id, { :logins => 1 }
+    end
   end
 
   def destroy
