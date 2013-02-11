ecd8c892db8314887f86fe6ca970cfdf718c93e3
James Peret <james.peret@gmail.com>
2013-02-11 13:51:53 -0200

    Mixpanel changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super
     if Rails.env.production?
-      mixpanel.track 'User Signup', { :user_id => current_user.id }
       username = current_user.first_name + " " + current_user.last_name
       mixpanel.set current_user.id, { :name => username, :email => current_user.email}
-      mixpanel.increment current_user.id, { :logins => 1 }      
+      mixpanel.increment current_user.id, { :logins => 1 }
+      mixpanel.track 'User Signup', { :distinct_id => current_user.id }
     end
   end
 
