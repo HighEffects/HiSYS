afe37d5df205a7d26c92754b3759bc091abb3c0f
James Peret <james.peret@gmail.com>
2015-04-20 22:19:23 -0300

    AWS Security issue fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                                      # required
-    :aws_access_key_id      => 'AKIAJJNMLLRSRLIKPF6Q',                     # required
-    :aws_secret_access_key  => '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', # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                     # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], # required
     :region                 => 'sa-east-1',                                # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'higheffects'                     # required
